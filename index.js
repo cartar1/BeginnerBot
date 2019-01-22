341fcbe483ab9618034344f687367b8786e359ee
cartar1 <39052179+cartar1@users.noreply.github.com>
2019-01-22 20:51:13 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const prefix = "!"
+
+bot.on('ready', () => {
+     
+    bot.user.setActivity("with you!");
+    bot.user.setStatus("dnd");
+    
+});
+
+bot.on("message", async message => {
+  
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    if(message.author.bot) return;
+    const command = args.shift().toLowerCase();
+    const sender = message.author;
+    if(message.channel.type === "dm") return;
+    if(!message.content.startsWith(prefix)) return;
+  
+    if(command === "foo") {
+    
+       message.channel.send("Bar!");
+    
+      return;
+    }
+  
+    if(command === "embed") {
+    
+      const embed = new Discord.RichEmbed()
+      .setTitle("Embed")
+      .setColor("RANDOM")
+      .addField("Field Title", "Field description.")
+      
+      message.channel.send(embed);
+    
+      return;
+    }
+  
+});
